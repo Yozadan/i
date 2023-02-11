092da4d689d55a2e1ff7fdfe50622c90da2cbfa3
Yozadan <98656290+Yozadan@users.noreply.github.com>
2023-02-11 13:08:36 -0500

    Add files via upload

---
+++ bedroom.js
@@ -0,0 +1,18 @@
+img = "";
+function setup(){
+canvas = createCanvas(640, 420);
+canvas.center();
+}
+
+function preload(){
+img = loadImage('');
+}
+
+function draw(){
+image(img, 0, 0, 640, 420);
+fill("#FF0000");
+text("", 45, 35);
+noFill();
+stroke("#FF0000");
+rect(30,60,450,350);
+}
